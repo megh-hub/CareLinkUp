59086cb7406e97696194f7fc52ad3430d21f15f5
rujulsrivastava <rujulsrivastava@gmail.com>
2022-12-10 23:35:19 +0530

    blockchain

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+let BlockChain = require("./blockchain/blockChain");
+let blockChain = new BlockChain();
+let hash = require('object-hash');
+let PROOF = 1560;
+
+let validProof = (proof) => {
+  let guessHash = hash(proof);
+  console.log("hash: ", guessHash);
+  return guessHash == hash(PROOF);
+}
+
+let proofOfWork = () => {
+  let proof = 0;
+  while (true) {
+    if (!validProof(proof)) {
+      proof++;
+    } else {
+      break;
+    }
+  }
+  return proof;
+}
+
+if (proofOfWork()==PROOF){
+  blockChain.addNewTransaction("person1", "person2", 300);
+  let prevHash = blockChain.lastBlock() ? blockChain.lastBlock().hash : null;
+  blockChain.addNewBlock(prevHash);
+}
+
+console.log("chain: ", blockChain.chain);
+
 $(document).ready(function () {
     $("#nav").load("header.txt");
   });
+
