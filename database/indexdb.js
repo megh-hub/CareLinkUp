a2fced966fe2073302ae3abebbc2ffedc9ec471e
rujulsrivastava <rujulsrivastava@gmail.com>
2022-12-11 02:06:14 +0530

    wait

--- database/indexdb.js
+++ database/indexdb.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
+let BlockChainModel = require("./model");
 mongoose.connect("mongodb://localhost:2701/blockChain", (err) => {
     if (err) return console.log("Cannot connect");
     console.log("DB Connected");
 });
 
 let ConnectionCallBack = () => {};
+
+module.exports.onConnect = (callback) => {
+    ConnectionCallBack = callback;
+}
